eadfc9df731e428e2d41ea6689fe9657c68eb850
jpawesome101 <jpawesome101@gmail.com>
2024-12-02 17:33:01 -0500

    hi jacob

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -7,6 +7,6 @@ public class Hardware extends DriveConstants{
     public DcMotor fl;
 
     public Hardware(HardwareMap hardwareMap) {
-        fl = hardwareMap.get(DcMotor.class, "front left");
+        fl = hardwareMap.get(DcMotor.class, "front");
     }
 }
