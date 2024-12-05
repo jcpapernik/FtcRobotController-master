ecaa25b5d73e4ec2234c0eaba3404ed6aa37ce2e
jpawesome101 <jpawesome101@gmail.com>
2024-12-05 17:54:05 -0500

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -6,10 +6,10 @@ public class Hardware extends DriveConstants
     public DcMotor leftFront, leftRear, rightRear, rightFront,  lift;
     public Hardware(HardwareMap hardwareMap)
     {
-      //  leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-    //    leftRear = hardwareMap.get(DcMotor.class, "leftRear");
-       // rightRear = hardwareMap.get(DcMotor.class, "rightRear");
-       // rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         lift = hardwareMap.get(DcMotor.class, "lift");
 
 
