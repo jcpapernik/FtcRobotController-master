40ef42a2584f0fd7ebb6916d10c0241a54393188
jpawesome101 <jpawesome101@gmail.com>
2024-12-16 17:10:37 -0500

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,31 +1,32 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class Hardware extends DriveConstants
-{
+public class Hardware extends DriveConstants {
     public DcMotor leftFront;
     public DcMotor leftRear;
     public DcMotor rightRear;
     public DcMotor rightFront;
-    public static DcMotor lift;
+    public  DcMotor lift;
     public DcMotor extend;
 
     public Servo outtake, intakeRotation;
-    public Hardware(HardwareMap hardwareMap, HardwareMap map)
-    {
+
+    public Hardware(HardwareMap hardwareMap) {
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         leftRear = hardwareMap.get(DcMotor.class, "leftRear");
         rightRear = hardwareMap.get(DcMotor.class, "rightRear");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         lift = hardwareMap.get(DcMotor.class, "lift");
         extend = hardwareMap.get(DcMotor.class, "extend");
-        outtake = hardwareMap.get(Servo.class, "outtake"); //servo
-        intakeRotation = hardwareMap.get(Servo.class, "intakeRotation"); //servo
+        outtake = hardwareMap.get(Servo.class, "outtake");
+        intakeRotation = hardwareMap.get(Servo.class, "intakeRotation");
 
-        //rotation intake, intake is a continuous serVo
-// TODO: 12/5/2024
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp
+public class MecanumTeleOp extends LinearOpMode {
+   // Hardware hardwareMap;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+       // HardwareMap HardwareMap;
+        Hardware drive = new Hardware(hardwareMap);
+
+
+
+        // See the note about this earlier on this page.
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double leftFrontPower = (y + x + rx) / denominator;
+            double leftRearPower = (y - x + rx) / denominator;
+            double rightFrontPower = (y - x - rx) / denominator;
+            double rightRearPower = (y + x - rx) / denominator;
+
+            drive.leftFront.setPower(leftFrontPower);
+            drive.leftRear.setPower(leftRearPower);
+            drive.rightFront.setPower(rightFrontPower);
+            drive.rightRear.setPower(rightRearPower);
+        }
+    }
+}
