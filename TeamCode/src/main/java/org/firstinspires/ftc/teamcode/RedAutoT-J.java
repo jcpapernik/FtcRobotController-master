d5457d813ce6ba72f65dac4d821ef9f558434434
JPollard559 <113484696+JPollard559@users.noreply.github.com>
2025-01-09 18:26:38 -0500

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoT-J.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+public class RedAutoT-J extends
+
+SensorHuskyLens {
+    private int red; //color ID
+    private boolean isActive; //we are on red team
+    private int redCount; //amount of red blocks
+    @Override
+    public void runOpMode ()
+    {
+        {
+
+    @Override
+    public RedAuto()
+            {
+                this.red = red;
+                this.isActive = isActive;
+                this.redCount = redCount;
+
+
+            }
+            @Override
+            public void setRed ( int red)
+            {
+                this.red = red;
+            }
+            @Override
+            public void getRed ()
+            {
+                return red;
+            }
+            @Override
+            public void setIsActive ( boolean isActive)
+            {
+                this.isActive = isActive;
+            }
+            @Override
+            public getIsActive()
+            {
+                isActive = true;
+                return isActive;
+            }
+            @Override
+            public void setRedCount ( int redCount)
+            {
+                this.redCount = redCount;
+            }
+            @Override
+            public getRedCount()
+            {
+                return redCount;
+            }
+
+
+        }
+    }
+}
+/*
+1. instance variables
+2. constructer
+3. accser method
+4. mutater methods
+5. equals
+6. .toString()
+7. utility methods
+ */
